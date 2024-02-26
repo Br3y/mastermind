acd26febc2758202498efcad67d87c79db65f415
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-02-26 19:18:40 +0800

    feat: setup var array color to insert 8 primary colors

---
+++ mastermind.rb
@@ -0,0 +1,3 @@
+# setup var array color to insert 8 primary colors from index 0 to 7
+# it will be both used by codemaker and codebreaker
+color = ["red", "blue", "yellow", "green", "purple", "orange", "brown", "black"]
