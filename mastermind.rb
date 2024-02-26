911b4021db32788aa4f5d4d872950af00cbf3950
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-02-26 19:30:35 +0800

    feat: create var secret_code and guess
    Variable will be both used by codemaker and codebreaker later.

--- mastermind.rb
+++ mastermind.rb
@@ -1,3 +1,7 @@
 # setup var array color to insert 8 primary colors from index 0 to 7
 # it will be both used by codemaker and codebreaker
 color = ["red", "blue", "yellow", "green", "purple", "orange", "brown", "black"]
+# set and insert 4 secret_code for codemaker
+secret_code = []
+# set guess to 1, max of 12 guess for codebreaker to win the game 
+guess = 1
